fbe94e7e977033fb45d1637473cfe1e22dd7ee8d
dmitry <dmitry@dmitry-N551JM>
2018-09-13 09:13:12 +0300

    Employee

--- FirstHibernateProject/src/main/java/models/Employee.java
+++ FirstHibernateProject/src/main/java/models/Employee.java
@@ -1,55 +1,32 @@
 package main.java.models;
 
-import javax.persistence.*;
 
-@Entity
-@Table(name = "EMPLOYEE")
 public class Employee {
+   private int id;
+   private String first_name; 
+   private String last_name;   
+   private int salary;  
 
-    @Id @GeneratedValue
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "salary")
-    private int salary;  
-
-    public Employee() {}
-    
-    public int getId() {
-       return id;
-    }
-    
-    public void setId( int id ) {
-       this.id = id;
-    }
-    
-    public String getFirstName() {
-       return firstName;
-    }
-    
-    public void setFirstName( String first_name ) {
-       this.firstName = first_name;
-    }
-    
-    public String getLastName() {
-       return lastName;
-    }
-    
-    public void setLastName( String last_name ) {
-       this.lastName = last_name;
-    }
-    
-    public int getSalary() {
-       return salary;
-    }
-    
-    public void setSalary( int salary ) {
-       this.salary = salary;
-    }
+   public Employee() {}
+   public Employee(String fname, String lname, int salary) {
+      this.first_name = fname;
+      this.last_name = lname;
+      this.salary = salary;
+   }
+   
+   public int getId() {
+      return id;
+   }
+   
+   public String getFirstName() {
+      return first_name;
+   }
+   
+   public String getLastName() {
+      return last_name;
+   }
+   
+   public int getSalary() {
+      return salary;
+   }
 }
